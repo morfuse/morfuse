3fc3faa74b55b541f3ef92fbd4710c2677c4c35d
L <l>
2021-10-27 19:49:36 +0200

    Added namespace event test

--- tests/namespace.cpp
+++ tests/namespace.cpp
@@ -9,11 +9,43 @@ using namespace mfuse;
 NamespaceDef namespace1("namespace1", "test");
 NamespaceDef namespace2("namespace2", "test");
 
+class InNamespace : public SimpleEntity {
+public:
+	MFUS_CLASS_PROTOTYPE(InNamespace);
+
+	InNamespace()
+		: wasCalled(false)
+	{}
+
+	void TestEvent1(Event& ev)
+	{
+		std::cout << "Successfully called" << std::endl;
+		wasCalled = true;
+	}
+
+public:
+	bool wasCalled;
+};
+
+class OutNamespace : public SimpleEntity {
+public:
+	MFUS_CLASS_PROTOTYPE(OutNamespace);
+
+	void TestEvent2(Event& ev)
+	{
+		// shouldn't be called
+		throw;
+	}
+};
+
 PredefinedString entString("ent");
 
 const char scriptData1[] =
 "local.test[0] = spawn InNamespace targetname ent\n"
 "local.test[1] = spawn OutNamespace targetname ent\n"
+"local.test[0] test_event1\n"
+"local.test[0] test_event2\n"
+"if (local.test[1]) local.test[1] test_event2\n"
 "end";
 
 void level1(ScriptMaster& director)
@@ -48,6 +80,9 @@ void includeTest()
 	const ConTarget* targets = list.GetExistingConstTargetList(entString);
 	assertTest(targets);
 	assertTest(targets->NumObjects() == 1);
+
+	InNamespace* ent = static_cast<InNamespace*>(targets->ObjectAt(1).Pointer());
+	assert(ent->wasCalled);
 }
 
 void excludeTest()
@@ -71,6 +106,9 @@ void excludeTest()
 	const ConTarget* targets = list.GetExistingConstTargetList(entString);
 	assertTest(targets);
 	assertTest(targets->NumObjects() == 1);
+
+	InNamespace* ent = static_cast<InNamespace*>(targets->ObjectAt(1).Pointer());
+	assert(ent->wasCalled);
 }
 
 int main()
@@ -80,22 +118,32 @@ int main()
 	return 0;
 }
 
-class InNamespace : public SimpleEntity {
-public:
-	MFUS_CLASS_PROTOTYPE(InNamespace);
-};
+EventDef evInNamespace(
+	namespace1,
+	"test_event1",
+	0,
+	"",
+	"",
+	""
+);
 
-class OutNamespace : public SimpleEntity {
-public:
-	MFUS_CLASS_PROTOTYPE(OutNamespace);
-};
+EventDef evOutNamespace(
+	namespace2,
+	"test_event2",
+	0,
+	"",
+	"",
+	""
+);
 
 MFUS_CLASS_DECLARATION_NAMESPACE(namespace1, SimpleEntity, InNamespace, nullptr)
 {
+	{ &evInNamespace, & InNamespace::TestEvent1 },
 	{ nullptr, nullptr }
 };
 
 MFUS_CLASS_DECLARATION_NAMESPACE(namespace2, SimpleEntity, OutNamespace, nullptr)
 {
+	{ &evOutNamespace, & OutNamespace::TestEvent2 },
 	{ nullptr, nullptr }
 };
