4549571746cefc6f3a0e7ed951bbea2b98a473d8
L <private@private.com>
2022-08-16 15:41:38 +0200

    Added continue later test

--- tests/script_wait_thread.cpp
+++ tests/script_wait_thread.cpp
@@ -20,6 +20,17 @@ const char scriptContent_1[] =
 "println \"end t2\"\n"
 "end\n";
 
+const char scriptContent_2[] =
+"main:\n"
+"thread continuelater local\n"
+"pause\n"
+"println ok\n"
+"end 1\n"
+"continuelater local.thread:\n"
+"wait 0.05\n"
+"local.thread wait 0\n"
+"end";
+
 void script1(ScriptMaster& director)
 {
 	// Compile the script and return the game script object
@@ -28,23 +39,44 @@ void script1(ScriptMaster& director)
 	// Execute the thread from the beginning of the script
 	Event parms;
 	director.ExecuteThread(script, parms);
+
+	// Execute all pending events
+	ScriptContext& ctx = ScriptContext::Get();
+	const TimeManager& tm = ctx.GetTimeManager();
+	while (!ctx.IsIdle() && tm.GetTime() < 1000) {
+		ctx.Execute();
+	}
+}
+
+void script2(ScriptMaster& director)
+{
+	// Compile the script and return the game script object
+	const ProgramScript* const script = compile(director, "script2", scriptContent_2);
+
+	// Execute the thread from the beginning of the script
+	Event parms;
+	director.ExecuteThread(script, parms);
+
+	// Execute all pending events
+	ScriptContext& ctx = ScriptContext::Get();
+	const TimeManager& tm = ctx.GetTimeManager();
+	while (!ctx.IsIdle() && tm.GetTime() < 1000) {
+		ctx.Execute();
+	}
+
+	assert(parms.GetInteger(1) == 1);
 }
 
 const handler_t list[] =
 {
-	"script1", &script1
+	{ "script1", &script1 },
+	{ "script2", &script2 }
 };
 
 int main()
 {
 	ScriptContext context;
 	testAll(context, list);
-
-	// Execute all pending events
-	const TimeManager& tm = context.GetTimeManager();
-	while (!context.IsIdle() && tm.GetTime() < 1000) {
-		context.Execute();
-	}
 
 	assertTest(context.IsIdle());
 
