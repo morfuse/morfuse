171d466d3799599b624c360768d2fe3502f170bc
smallmodel <15067410+smallmodel@users.noreply.github.com>
2026-02-27 23:38:35 +0100

    fix(parser): parse end keyword independently

--- tests/script_syntax.cpp
+++ tests/script_syntax.cpp
@@ -21,6 +21,15 @@ const char scriptContent_level2[] =
 "assert(local    .3value == 3)\n"
 "end\n";
 
+const char scriptContent_level3[] =
+"main:\n"
+"  local.end = 2\n"
+"  local.value = local.end//test - value, with comment\n"
+"  println (\"level3_value: \" + local.value)\n"
+"  local end//test - value, with comment\n"
+"  local.value = int end//test - value, with comment\n"
+"  end//test - value, with comment\n";
+
 void level1(ScriptMaster& director)
 {
     const ProgramScript* const script = compile(director, "level1", scriptContent_level1);
@@ -37,10 +46,19 @@ void level2(ScriptMaster& director)
     director.ExecuteThread(script, parms);
 }
 
+void level3(ScriptMaster& director)
+{
+    const ProgramScript* const script = compile(director, "level3", scriptContent_level3);
+
+    Event parms;
+    director.ExecuteThread(script, parms);
+}
+
 handler_t handlers[] =
 {
     { "level1", &level1 },
-    { "level2", &level2 }
+    { "level2", &level2 },
+    { "level3", &level3 }
 };
 
 int main()
